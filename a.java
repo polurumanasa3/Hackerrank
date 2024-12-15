1a0166d4cd2bf54f12609bcf890b5ec464fd6b59
polurumanasa3 <manasap142@gmail.com>
2024-12-14 22:25:33 -0800

    Add files via upload

---
+++ a.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		if(x >= 2 * y){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+		
+
+	}
+}
